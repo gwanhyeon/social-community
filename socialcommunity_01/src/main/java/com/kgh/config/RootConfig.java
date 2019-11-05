e50bddada7c88b2f74545308ce3d9db0de47cab1
kgh940525 <kgh940525@gmail.com>
2019-11-06 05:26:58 +0900

    Add Annotation, junit Test, DI

---
+++ socialcommunity_01/src/main/java/com/kgh/config/RootConfig.java
@@ -0,0 +1,11 @@
+package com.kgh.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages="com.kgh.cook")
+public class RootConfig {
+	
+
+}
